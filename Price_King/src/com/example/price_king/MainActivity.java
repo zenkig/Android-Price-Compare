611b047547981d55aacb08ea8359dd6e3ad308a4
SoumyaParida <soumya.parida3@gmail.com>
2014-02-20 17:45:58 +0100

    New Price comparator code

--- Price_King/src/com/example/price_king/MainActivity.java
+++ Price_King/src/com/example/price_king/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.price_king;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
@@ -20,4 +23,16 @@ public class MainActivity extends Activity {
         return true;
     }
     
-}
+    /** Called when the user clicks the Send button */
+    /*public void search(View view) {
+        // Do something in response to button
+    	Intent intent = new Intent(this, DisplayMessageActivity.class); 
+    	EditText editText = (EditText) findViewById(R.id.editText1);
+    	String message = editText.getText().toString();
+    	final String EXTRA_MESSAGE = "Give your Search";
+    	intent.putExtra(EXTRA_MESSAGE, message);
+    }*/
+    
+   
+    }
+
